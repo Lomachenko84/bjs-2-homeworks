9b4019d0ba0262f58ca48f6ade230e21f6f1137e
Lomachenko84 <agent84@inbox.ru>
2022-07-16 22:13:23 +0300

    дз к занятию 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,15 +1,33 @@
 // Задание 1
-function getArrayParams(arr) {
+function getArrayParams(arr) { //99, 50, -3
   let min, max, sum, avg;
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  avg = 0;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum = sum + arr[i];
 
-  return { min: min, max: max, avg: avg };
+  }
+  avg = sum / arr.length;
+
+  return { min: min, max: max, avg: avg};
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
 
   // Ваш код
 
@@ -18,14 +36,30 @@ function worker(arr) {
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
-  
+  max = -Infinity
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let res = func(arrOfArr[i]);
+    if (res > max) {
+      max = res;
+    }
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min, max, difference
+  min = Infinity;
+  max = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  difference = max - min
+  return Math.abs(difference);
 }
