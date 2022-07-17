7cefc9817718ef54a14bb1842e71ce4915ca362b
Lomachenko84 <agent84@inbox.ru>
2022-07-17 18:11:11 +0300

    дз 4 к заданию объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,44 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.subject = null;
+  this.averagescore = null// Ваш код
+  this.expelled = null// Ваш код
 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName; //ваш код
 }
-
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+    // добавить первую оценку 
+  } else {
+    this.marks.push(mark);
+    //puc добавить вторую и последующие оценки в массив
+  }
+}
+Student.prototype.addMarks = function (...theMarks) {
+  if (this.marks === undefined) {
+    this.marks = [...theMarks];
+    // добавить первую оценку 
+  } else {
+    this.marks.push(...theMarks);
+  }
+}
+Student.prototype.getAverage = function () {
+  let sum
+  sum = 0
+  for (let i = 0; i < this.marks.length; i++) {
+    sum = sum + this.marks[i];
+  }
+  let avg = sum / this.marks.length
+  return avg;
+}
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
