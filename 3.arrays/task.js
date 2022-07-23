36e2ef3dc2a016dd3a0312021fbf66ccb261b2a0
Lomachenko84 <agent84@inbox.ru>
2022-07-23 21:23:03 +0300

    исправленное дз

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,26 +1,10 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  function n(element, index) {
-    if (element === arr2[index]) return true;
-    if (element !== arr2[index]) return false;
-  }
-  if (arr1.length !== arr2.length) return false;
-
-  result = arr1.every(n)
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
 
 
 function advancedFilter(arr) {
-  let resultArrlet = arr.filter(function (elem) {
-    return elem % 3 == 0 
-  });
-  let d = resultArrlet.filter(function (i) {
-    return i > 0
-  });
-  let map = d.map(function (elem) {
-    return elem * 10
-  });
-  return map;
+  let resultArrlet = arr.filter( (elem) =>  elem % 3 == 0).filter((i) => i > 0).map((elem) => elem * 10);
+  return resultArrlet
 }
